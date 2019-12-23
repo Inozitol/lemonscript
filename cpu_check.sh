0040892bf210549719e32910fe056f591928d346
Pavel Horacek <horacek.pavel@protonmail.com>
2019-12-23 16:13:59 +0100

    New file for CPU tracking

---
+++ cpu_check.sh
@@ -0,0 +1,6 @@
+OUT_FILE="/home/$USER/cpustat"
+
+while true; do
+        CPUPERC=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1); }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat))
+        echo "${CPUPERC%.*}%" > $OUT_FILE
+done
